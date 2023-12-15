9f90b43ce18f4cd98f53cb6ea2cacefd473b1841
Alexr42 <alexr42.radu@gmail.com>
2023-12-15 09:48:22 +0100

    index.js; ej3

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 let arrayVacio = [];
 
 //2.- Crear variable de nombre arrayNumeros declarada con un array de números del 0 al 9 (0, 1, 2...)
-let arrayNumeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+let arrayNumeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 //3.- Crear variable de nombre arrayNumerosPares declarada con un array con los números pares del 0 al 9 (considerando al 0 par)
-
+let arrayNumerosPares = [0, 2, 4, 6, 8];
 
 //4.- Crear variable de nombre arrayBidimensional declarada con valor array [[0, 1, 2], ['a', 'b', 'c']]
 
